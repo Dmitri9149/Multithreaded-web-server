015a2bc73ab32d6b1a719869f311d5fc0780661e
dmitri9149 <dmitri.as.name@gmail.com>
2020-07-21 20:18:24 +0300

    corresponding changes in main fn

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
 }
 
