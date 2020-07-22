b5e55947531a4ab93cd44b35cc26653602ce6880
dmitri9149 <dmitri.as.name@gmail.com>
2020-07-22 17:38:55 +0300

    create vector to hold the threads

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
+use std::thread;
 
-pub struct ThreadPool;
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -12,7 +15,13 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        ThreadPool
+        let mut threads = Vec::with_capacity(size);
+
+        for _ in 0..size {
+            // create some threads and store them in the vector
+        }
+
+        ThreadPool { threads }
     }
 
 
